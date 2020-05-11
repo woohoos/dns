544bdd631981441c5510bb5fff3dbbdba934d981
woohoos <65131615+woohoos@users.noreply.github.com>
2020-05-11 16:46:59 +0300

    Add files via upload

---
+++ DnsClient.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <winsock2.h>
+
+#define BUFFLEN 1024
+
+int main(int argc, char *argv[]){
+    WSADATA data;
+    int sock;
+    struct hostent *hp;
+    struct sockaddr_in servaddr;
+    char buffer[BUFFLEN];
+
+    if (argc != 3){
+        printf("Naudojimas: %s <hostname> <port>\n",argv[0]);
+        return -1;
+    }
+
+    WSAStartup(MAKEWORD(2,2),&data);
+    hp = gethostbyname(argv[1]);
+    sock = socket(AF_INET, SOCK_STREAM,0);
+    memset(&servaddr,0,sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(atoi(argv[2]));
+    memcpy((char *)&servaddr.sin_addr, hp->h_addr, hp->h_length);
+    connect(sock,(struct sockaddr*)&servaddr,sizeof(servaddr));
+
+    memset(&buffer,0,BUFFLEN);
+    printf("Enter the message: ");
+    fgets(buffer, BUFFLEN-4, stdin);
+    strcat(buffer,"\r\n\r\n");
+    send(sock,buffer,strlen(buffer),0);
+    memset(&buffer,0,BUFFLEN);
+    recv(sock,buffer,BUFFLEN,0);
+    printf("Server sent: %s\n", buffer);
+    closesocket(sock);
+    return 0;
+}
